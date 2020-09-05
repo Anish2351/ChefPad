becc37100f7244a560c9ea5f0401e11bab1745de
Anish2351 <anishgupta54321@gmail.com>
2020-09-05 10:49:46 +0530

    Initials

---
+++ android/app/src/main/kotlin/com/anish/recipes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anish.recipes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,275 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:recipes/models/recipe_model.dart';
+import 'package:recipes/views/recipe_view.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<RecipeModel> recipes = List<RecipeModel>();
+  TextEditingController textEditingController = TextEditingController();
+  getRecipes(String query) async {
+    setState(() {});
+    recipes.clear();
+    String url =
+        "https://api.edamam.com/search?q=$query&app_id=ee120dd2&app_key=f381f885eba73d0f5dc03f23be673a7c";
+    var response = await http.get(url);
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+
+    jsonData["hits"].forEach((element) {
+      RecipeModel recipeModel = RecipeModel();
+      recipeModel = RecipeModel.fromMap(element['recipe']);
+      recipes.add(recipeModel);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    const Color(0xff213A50),
+                    const Color(0xff071930),
+                  ],
+                  begin: FractionalOffset.topRight,
+                  end: FractionalOffset.bottomLeft),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                  vertical: !kIsWeb ? Platform.isIOS ? 60 : 30 : 30,
+                  horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: kIsWeb
+                        ? MainAxisAlignment.start
+                        : MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "AppGuy",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Overpass'),
+                      ),
+                      Text(
+                        "Recipes",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Overpass'),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Text(
+                    "What will you cook today?",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Overpass'),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Just Enter Ingredients you have and we will show the best recipe for you",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: 'OverpassRegular'),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: textEditingController,
+                            decoration: InputDecoration(
+                              hintText: "Enter Ingredients",
+                              hintStyle: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white.withOpacity(0.5),
+                                fontFamily: 'Overpass',
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontFamily: 'Overpass',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        InkWell(
+                          onTap: () async {
+                            if (textEditingController.text.isNotEmpty) {
+                              getRecipes(textEditingController.text);
+                            }
+                          },
+                          child: Container(
+                            child: Icon(
+                              Icons.search,
+                              size: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    child: GridView(
+                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                          mainAxisSpacing: 10.0, maxCrossAxisExtent: 200.0),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      physics: ClampingScrollPhysics(),
+                      children: List.generate(
+                        recipes.length,
+                        (index) {
+                          return GridTile(
+                            child: RecipeTile(
+                              title: recipes[index].label,
+                              imgUrl: recipes[index].image,
+                              desc: recipes[index].source,
+                              url: recipes[index].url,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RecipeTile extends StatefulWidget {
+  final String title, desc, imgUrl, url;
+  RecipeTile({this.title, this.desc, this.imgUrl, this.url});
+  @override
+  _RecipeTileState createState() => _RecipeTileState();
+}
+
+class _RecipeTileState extends State<RecipeTile> {
+  _launchURL(String url) async {
+    print(url);
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        GestureDetector(
+          onTap: () {
+            if (kIsWeb) {
+              _launchURL(widget.url);
+            } else {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RecipeView(
+                    postUrl: widget.url,
+                  ),
+                ),
+              );
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.all(8),
+            child: Stack(
+              children: <Widget>[
+                Image.network(
+                  widget.imgUrl,
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  width: 200,
+                  alignment: Alignment.bottomLeft,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.white30, Colors.white],
+                        begin: FractionalOffset.centerRight,
+                        end: FractionalOffset.centerLeft),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          widget.title,
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.black54,
+                              fontFamily: 'Overpass'),
+                        ),
+                        Text(
+                          widget.desc,
+                          style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.black54,
+                              fontFamily: 'OverpassRegular'),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
